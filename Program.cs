bc2d78474c942ee66e2d29d8a33e37d6ccbbfd22
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2022-07-17 11:04:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            
+
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerservice = new FileLoggerService();
+            ILoggerService smslogger = new SmsLoggerService();
+
+
+
+            BasvuruManager basvuruManager=new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager,smslogger);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasıtKrediManager };
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
